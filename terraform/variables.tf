9589cc90945408a3b221bb29148cc09b8f0c3327
Oppilop <nicolepolippo18@gmail.com>
2026-09-11 20:31:46 -0300

    chore: k8s 1.31

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,7 +29,7 @@ variable "cluster_name" {
 
 variable "kubernetes_version" {
   type    = string
-  default = "1.30"
+  default = "1.31"
 }
 
 # ─────────────────────────────────────────────────────────────────────────────
